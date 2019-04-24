1e802eb7573f46248445128dcc591a5db0ea5e34
shuai.jin <shuai.jin@mfashion.com.cn>
2019-04-24 20:44:34 +0800

    Gateway负载均衡

--- springcloud-serverProvider/src/main/java/com/jin/controller/HelloController.java
+++ springcloud-serverProvider/src/main/java/com/jin/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.jin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/provider1")
 public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam String name) {
-		return "hello " + name; 
+		return "hello " + name + "            11111111111111"; 
+	}
+	
+	@GetMapping("/getProperty")
+	public String getProperty(@RequestParam String foo) {
+		return foo + "                        1111111111111111111";
 	}
 }
