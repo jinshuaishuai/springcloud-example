dbe951f0a7125d1cbce2446e889b022d5448bf62
shuai.jin <shuai.jin@mfashion.com.cn>
2019-04-24 11:31:49 +0800

    新增功能

--- springcloud-serverConsumer/src/main/java/com/jin/ConsumerApplication.java
+++ springcloud-serverConsumer/src/main/java/com/jin/ConsumerApplication.java
@@ -2,13 +2,31 @@ package com.jin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 @SpringBootApplication
-@EnableDiscoveryClient	//启动注册发现功能
-@EnableFeignClients		//开启Feign功能。
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
+	
+	@Bean
+	  public ServletRegistrationBean getServlet() {
+	    HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+	    ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+	    registrationBean.setLoadOnStartup(1);
+	    registrationBean.addUrlMappings("/hystrix.stream");
+	    registrationBean.setName("HystrixMetricsStreamServlet");
+	    return registrationBean;
+	  }
 }
---
+++ springcloud-serverProvider/src/main/java/com/jin/entity/AO/BookAO.java
@@ -0,0 +1,16 @@
+package com.jin.entity.AO;
+
+import lombok.Data;
+
+@Data
+public class BookAO {
+	
+	private String bookName;
+	
+	private Integer bookPrice;
+	
+	private String address;
+	
+	private String phone;
+	
+}
