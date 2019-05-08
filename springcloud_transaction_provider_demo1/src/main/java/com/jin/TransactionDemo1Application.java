a01178b2834e4ca9156f653956008aa85ee76a5a
jinshuaishuai <jin2111843364@163.com>
2019-05-08 23:19:11 +0800

    测试进行代码提交

--- springcloud_transaction_provider_demo1/src/main/java/com/jin/TransactionDemo1Application.java
+++ springcloud_transaction_provider_demo1/src/main/java/com/jin/TransactionDemo1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableDiscoveryClient
+
 public class TransactionDemo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionDemo1Application.class, args);
