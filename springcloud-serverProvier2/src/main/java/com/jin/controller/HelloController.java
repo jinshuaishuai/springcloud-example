261ed2c63347d94b29f13ae0a1382810d042ed6f
shuai.jin <shuai.jin@mfashion.com.cn>
2019-05-27 11:49:30 +0800

    解决gateway的熔断转发错误

--- springcloud-serverProvier2/src/main/java/com/jin/controller/HelloController.java
+++ springcloud-serverProvier2/src/main/java/com/jin/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.jin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,10 @@ public class HelloController {
 	@GetMapping("/getProperty")
 	public String getProperty(@RequestParam String foo) {
 		return foo + "             2222222222222";
+	}
+	
+	@PostMapping("/context")
+	public String context() {
+		return "可以 22222222222";
 	}
 }
