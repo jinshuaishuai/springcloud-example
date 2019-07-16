5df77c4669ee7d0edfc45eb52dd439dceed530bd
shuai.jin <shuai.jin@mfashion.com.cn>
2019-07-16 16:56:18 +0800

    update

---
+++ springcloud-commons/src/main/java/com/jin/common/config/AliLogConfig.java
@@ -0,0 +1,13 @@
+package com.jin.common.config;
+
+public class AliLogConfig {
+	public static final String ENDPOINT = "xxx";
+	
+	public static final String  ACCESSKEYID = "xxx";
+	
+	public static final String ACCESSKEYSECRET = "xxx";
+	
+	public static final String PROJECT = "xxx";
+	
+	public static final String LOGSTORE = "xxx";
+}
