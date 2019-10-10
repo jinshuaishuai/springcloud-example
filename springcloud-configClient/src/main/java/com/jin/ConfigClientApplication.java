1294098346882cf238e35b4e6a637dcdc272e435
shuai.jin <shuai.jin@mfashion.com.cn>
2019-10-10 16:00:57 +0800

    update

--- springcloud-configClient/src/main/java/com/jin/ConfigClientApplication.java
+++ springcloud-configClient/src/main/java/com/jin/ConfigClientApplication.java
@@ -7,10 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient		//开启服务发现
 public class ConfigClientApplication {
-	
 	public static void main(String[] args) {
-		
 		SpringApplication.run(ConfigClientApplication.class, args);
-	
 	}
 }
